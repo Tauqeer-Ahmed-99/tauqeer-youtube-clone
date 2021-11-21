14e14c9da9348a85ca95c693b38a379bc2daae3d
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2021-11-21 17:00:30 +0530

    react roter setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
 import React, { useState } from "react";
+
 import { Container } from "react-bootstrap";
+
 import Header from "./components/header/Header";
 import SideBar from "./components/sideBar/SideBar";
 import HomeScreen from "./screens/homeScreen/HomeScreen";
 import LoginScreen from "./screens/loginScreen/LoginScreen";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
 import "./_app.scss";
 
-function App() {
-  // const [sideBar, toggleSideBar] = useState(false);
+const Layout = ({ children }) => {
+  const [sideBar, toggleSideBar] = useState(false);
 
-  // const handleToggleSideBar = () => {
-  //   toggleSideBar((prevState) => !prevState);
-  // };
-
+  const handleToggleSideBar = () => {
+    toggleSideBar((prevState) => !prevState);
+  };
   return (
-    // <>
-    //   <Header onToggleSideBar={handleToggleSideBar} />
-    //   <div className="app__container">
-    //     <SideBar
-    //       onToggleSideBar={sideBar}
-    //       handleToggleSideBar={handleToggleSideBar}
-    //     />
-    //     <Container fluid className="app__main">
-    //       <HomeScreen />
-    //     </Container>
-    //   </div>
-    // </>
-    <LoginScreen />
+    <>
+      <Header onToggleSideBar={handleToggleSideBar} />
+      <div className="app__container">
+        <SideBar
+          onToggleSideBar={sideBar}
+          handleToggleSideBar={handleToggleSideBar}
+        />
+        <Container fluid className="app__main">
+          {children}
+        </Container>
+      </div>
+    </>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Layout>
+            <HomeScreen />
+          </Layout>
+        </Route>
+
+        <Route path="/auth">
+          <LoginScreen />
+        </Route>
+
+        <Route path="/search">
+          <Layout>
+            <h1>Search Results</h1>
+          </Layout>
+        </Route>
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
