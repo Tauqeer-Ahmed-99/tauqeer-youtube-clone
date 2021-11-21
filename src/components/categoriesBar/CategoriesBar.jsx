a44de05affafa82ff185f1fd9262d781491b18ef
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2021-11-21 15:08:07 +0530

    added categories bar and home screen ui

--- src/components/categoriesBar/CategoriesBar.jsx
+++ src/components/categoriesBar/CategoriesBar.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
 import "./_categoriesBar.scss";
 
+const keywords = [
+  "All",
+  "React Js",
+  "javascript",
+  "computer applications",
+  "comedies",
+  "scene",
+  "history",
+  "conversation",
+  "street food",
+  "podcasts",
+  "recently uploaded",
+  "watched",
+  "new to you",
+];
+
 const CategoriesBar = () => {
-  return <div>Categories Bar</div>;
+  const [activeKeyword, setActiveKeyword] = useState("All");
+
+  const onClickHandler = (keyword) => {
+    setActiveKeyword(keyword);
+  };
+
+  return (
+    <div className="categories_bar">
+      {keywords.map((keyword, i) => (
+        <span
+          key={i}
+          onClick={onClickHandler.bind(null, keyword)}
+          className={activeKeyword === keyword ? "active" : ""}
+        >
+          {keyword}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default CategoriesBar;
